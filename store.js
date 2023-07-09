3c247939295568e507e86834f91ab4d313111c73
divyagupta2493 <129419450+divyagupta2493@users.noreply.github.com>
2023-07-09 16:32:54 +0530

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import habitsReducer from './features/habitsSlice'
-const store = configureStore({
-  reducer: {
-    allHabits: habitsReducer,
-  },
-})
-
-export default store
